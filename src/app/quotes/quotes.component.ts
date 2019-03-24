14c1ade5815a0686b2e212b5e173a15bc6f688b9
jackiegatwiri <jaciegatz@gmail.com>
2019-03-24 16:46:44 +0300

    add Upvote button

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,16 +9,25 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
   quotes =[ 
     new Quotes(1, "Failure will never overtake me if my determination to succeed is strong enough","Og mandino", "ACL"),
-    new Quotes(1, "With the new day comes new strenth and new thoughts.","Eleaner Roosevelt", "TLC"),
-    new Quotes(1, "Change your life today. Don't gamble on the future, act now, without delay.", "Simone de Beauior", "Disney"),
-    new Quotes(1, "Optisim is the faith that leads to achievement. Nothing can be done without hope and confidence.", "Hellen Keller", "HellenTT"),
-    new Quotes(1, "Good, better, best. Never let it rest. 'Til your good is better and your better is best.", "St. Jerome", "Brainy")
+    new Quotes(2, "With the new day comes new strenth and new thoughts.","Eleaner Roosevelt", "TLC"),
+    new Quotes(3, "Change your life today. Don't gamble on the future, act now, without delay.", "Simone de Beauior", "Disney"),
+    new Quotes(4, "Optisim is the faith that leads to achievement. Nothing can be done without hope and confidence.", "Hellen Keller", "HellenTT"),
+    new Quotes(5, "Good, better, best. Never let it rest. 'Til your good is better and your better is best.", "St. Jerome", "Brainy")
 ]
 
 
   constructor() { }
 
   ngOnInit() {
+  }
+  public upvote:number = 0;
+  public downvote:number = 0;
+  upvotes(){
+    return this.upvote ++;
+
+  }
+  downvotes(){
+    return this.downvote ++;
   }
 
 }
