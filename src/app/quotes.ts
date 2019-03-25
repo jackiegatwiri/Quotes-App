ba904ef360e4afb899a5128943722c33d92181a3
jackiegatwiri <jaciegatz@gmail.com>
2019-03-25 11:29:35 +0300

    highlight quotes

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -2,7 +2,7 @@ export class Quotes {
     public upvote:number;
     public downvote:number;
     constructor(public id:number, public description:string, public author:string, public publisher:string, public startDate:Date){
-        this.upvote=0
-        this.downvote=0
+        this.upvote=0;
+        this.downvote=0;
     }
 }
